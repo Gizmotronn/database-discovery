042f2dd9fb81f6d29c38a904fc91c6f9d3973089
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-03-26 14:57:53 +0800

    Delete todolistka.sql
    This isn't relevant to the assessment, but is still on the `master` branch

--- todolistka.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE todo_list (id INTEGER PRIMARY KEY, item TEXT, minutes INTEGER);
-INSERT INTO todo_list VALUES (1, "Wash the dishes", 15);
-INSERT INTO todo_list VALUES (2, "vacuuming", 20);
-INSERT INTO todo_list VALUES (3, "Learn some stuff on KA", 30);
-INSERT INTO todo_list VALUES (4, "Walk the dog", 55);
-
-SELECT SUM(minutes) FROM todo_list;
