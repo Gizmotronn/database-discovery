1a5e08a19ebb3c88f80a3ecb63bcd3345a85e62f
Liamaod <allianceofdroidsla@gmail.com>
2020-03-31 15:05:00 +0800

    Create igame-dmassessment-.sql
    dm assessment = data management assessment Y12 ATAR PMS

---
+++ igame-dmassessment-.sql
@@ -0,0 +1,7 @@
+/** file: iGame (3).accdb
+"list all customer names & full addresses for customers whose delivery suburb begins with "S".
+Mars One (5)
+Gridlinked (4)
+**/
+
+CREATE TABLE customer (customer_id INTEGER PRIMARY KEY, customerSurname TEXT, customerFirstName TEXT, customerEmail TEXT, customerPassword TEXT, customerOrderID INTEGER, customerCity TEXT, customerSuburb TEXT, customerMobilePhone INTEGER);
